0109841edf3f99ddc53d1953497c5f4bb0f52787
AdrielOlvera <82849600+AdrielOlvera@users.noreply.github.com>
2021-08-23 19:01:00 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,5 +1,36 @@
 def main():
     # Escribe el código adecuado para completar el programa
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+num3 = int(input("Ingresa el tercer número: "))
+if num1>num2<num3:
+    print(num2)
+    num2=num1+num3
+elif num2>num1<num3:
+    print(num1)
+    num1=num2+num3
+elif num2>num3<num1:
+    print(num3)
+    num3=num1+num2
+if num1>num2<num3:
+    print(num2)
+    num2=num1+num3
+elif num2>num1<num3:
+    print(num1)
+    num1=num2+num3
+elif num2>num3<num1:
+    print(num3)
+    num3=num1+num2
+if num1>num2<num3:
+    print(num2)
+    num2=num1+num3
+elif num2>num1<num3:
+    print(num1)
+    num1=num2+num3
+elif num2>num3<num1:
+    print(num3)
+    num3=num1+num2
+
     pass
 
 
